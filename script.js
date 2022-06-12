5b8c6c3cbc5124e8bf2219ed7da238541944a220
Albert <sharipov.albert1991@gmail.com>
2022-06-12 22:31:41 +0300

    add contact form

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const btnTheme = document.querySelector('#toogle-theme');
-const clickEvent = ('click' || 'touchstart');
+const modal = document.querySelector("#modal");
+const closeBtn = document.querySelector("#closeBtn");
+const contactUs = document.querySelector("#contact");
+let clickEvent = ('click' || 'touchstart' || 'touchend' || 'touchemove');
 
-btnTheme.addEventListener('click', function() {
+btnTheme.addEventListener(clickEvent, function() {
     document.body.classList.toggle('dark-theme'); 
+});
+
+closeBtn.addEventListener(clickEvent, function() {
+    modal.style.display = 'none';
+});
+
+contactUs.addEventListener(clickEvent, function() {
+    modal.style.display = 'block';
 });
